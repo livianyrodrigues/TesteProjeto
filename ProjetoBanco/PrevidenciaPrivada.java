2fa25458fa89262adf2c4e5eb7c75f4c10e8cd97
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-26 01:18:03 -0300

    Criado classe VIP

---
+++ ProjetoBanco/PrevidenciaPrivada.java
@@ -0,0 +1,21 @@
+
+/**
+ * Escreva a descrição da classe PrevidenciaPrivada aqui.
+ * 
+ * @author (seu nome) 
+ * @version (número de versão ou data)
+ */
+public class PrevidenciaPrivada extends ContaAbstrata{
+
+    private double aporte; 
+    
+
+    public void setAporte(double aporte) {
+        this.aporte = aporte;
+    }
+
+    public double getAporte() {
+        return aporte;
+    }
+
+}
