ee0666c63ed6f49ab53381ae797455d095cb386a
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-26 00:27:41 -0300

    Criado classe agencia

---
+++ ProjetoBanco/Agencia.java
@@ -0,0 +1,20 @@
+
+
+public class Agencia{
+    private int numero;
+    private String variacao;
+    
+    public int getNumero(){
+        return numero;
+    }
+    public void setNumero(int numero){
+        this.numero = numero;
+    }
+    public String getVariacao(){
+        return variacao;
+    }
+    public void setVariacao(String variacao){
+        this.variacao = variacao;
+    }
+    
+}
