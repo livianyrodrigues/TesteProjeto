effe36b0f0ba243a945077a2ae861ad6b0423995
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-26 01:08:04 -0300

    Versão.0.22

---
+++ ProjetoBanco/ContaAbstrata.java
@@ -0,0 +1,24 @@
+
+
+public abstract class ContaAbstrata implements ContaIF{
+    
+    private int numero;
+    private Agencia agencia;
+    
+   public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setAgencia(Agencia agencia) {
+        this.agencia = agencia;
+    }
+
+    public Agencia getAgencia() {
+        return agencia;
+    }
+    
+}
